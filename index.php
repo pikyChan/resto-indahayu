7dbe56aa8f6ad2269b17c3e8d3d1eb245aebe5a6
pikyChan <IndahAyu>
2023-11-14 10:38:11 +0700

    resto-indahayu

---
+++ index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Aplikasi Resto Enak</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <header>
+        <h1>Resto</h1>
+        <nav>
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Hot Promo</a></li>
+                <li><a href="#">Makanan</a></li>
+                <li><a href="#">Minuman</a></li>
+                <li><a href="#">Desert</a></li>
+            </ul>
+        </nav>
+</header>
+
+<section class="menu">
+    <h2>Menu Resto Enak</h2>
+    <div class="menu-item">
+        <img src="img/bakso-granat-mas-aziz.jpg" alt="menu 1">
+        <h3>Bakso Granat</h3>
+        <p>
+        Bakso Granat adalah salah satu kuliner bakso di Magelang yang telah mempunyai beberapa outlet/cabang di Magelang dan sekitarnya.
+        </p>
+        <p class="harga">Harga Rp. 10.000</p>
+        <button class="order-button">Pesan</button>
+    </div>
+</section>
+
+<section class="contact">
+    <h2>Hubungi Kami</h2>
+    <p>Jika ada pertanyaan, saran atau kritik hubungi kami di:</p>
+    <p>WA 085708935152</p>
+    <P>Email : admin@gmail.com</P>
+</section>
+
+<footer>
+    <p>&copy; 2023 Restoran Enak. Hak Cipta di Lindungi</p>
+</footer>
+</body>
+</html>
