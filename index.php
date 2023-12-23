f6ae90696c85494b814e8d05584c39a5b9741f8c
pikyChan <IndahAyu>
2023-12-24 05:41:22 +0700

    restoXI

--- index.php
+++ index.php
@@ -23,12 +23,26 @@
 <section class="menu">
     <h2>Menu Resto Enak</h2>
     <div class="menu-item">
-        <img src="img/bakso-granat-mas-aziz.jpg" alt="menu 1">
+        <img src="img/20231121081634bakso-granat-mas-aziz.jpg" alt="menu 1">
         <h3>Bakso Granat</h3>
         <p>
         Bakso Granat adalah salah satu kuliner bakso di Magelang yang telah mempunyai beberapa outlet/cabang di Magelang dan sekitarnya.
         </p>
+        <br>
         <p class="harga">Harga Rp. 10.000</p>
+        <br>
+        <button class="order-button">Pesan</button>
+    </div>
+    <div class="menu-item">
+        <img src="img/20231215102236012766200_1528175056-es_buah.jpg" alt="menu 2">
+        <h3>Es Buah</h3>
+        <p>
+        Segar dan Manis
+        </p>
+        <br>
+        <p class="harga">Harga Rp. 15.000 
+       <br>
+        <br>
         <button class="order-button">Pesan</button>
     </div>
 </section>
